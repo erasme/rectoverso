4001e39b2fef0e1abbaf21575180219e625d2575
kevinvennitti <kevin.vennitti@gmail.com>
2016-04-05 15:00:20 +0200

    Bugfix + better socket + documentation du code

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var server = require( 'http' ).createServer( app );
 var io = require( 'socket.io' )( server );
 var port = process.env.RECTO_VERSO_PORT || 5000;
 
+var players = [];
+var nbPlayersNeededToStart = 2;
+
 server.listen( port, function( ) {
     console.log( 'Server listening at port %d', port );
 } );
@@ -11,22 +14,96 @@ server.listen( port, function( ) {
 app.use( express.static( __dirname + '/public' ) );
 
 io.on( 'connection', function( socket ) {
-    console.log("connected");
+  
+	var id = new Date().getTime()+'-'+(~~(Math.random()*100));
+	
+  console.log('> New player \b \t  \b \t ID : '+id);
+  players.push({'id':id, 'readyToPlay':'false', 'score':0});
+  
+  socket.id = id;
+  
+  socket.emit('newPlayer', {id: socket.id});
+  
+  console_showPlayersArray();
+  
+  socket.on('readyToPlay', function(data) {
+    for (var i = 0; i < players.length; i++) {
+      if (players[i].id == socket.id) {
+        players[i].readyToPlay = true;
+      }
+    }
+    
+    console_showPlayersArray();
+    
+    var nbPlayersReady = 0;
+    
+    for (var i = 0; i < players.length; i++) {
+      if (players[i].readyToPlay == true) nbPlayersReady++;
+    }
+    
+    console.log('> '+nbPlayersReady+' player(s) ready to play!');
+    
+    if (nbPlayersReady == nbPlayersNeededToStart) {
+      console.log('> Let\'s start the game!');
+      io.sockets.emit('startGame',nbPlayersReady);        
+    }
+  });
+  
+  socket.on('foundPair', function (data) {
+    console.log('> Found pair! \b \t \b \t ID : '+socket.id, data);
+    
+    for (var i = 0; i < players.length; i++) {
+      /*
+       *  On incrémente le score
+       *  du joueur concerné
+       */
+       
+      if (players[i].id == socket.id) {
+        players[i].score++;
+        
+      } 
+    }
+    
+    io.sockets.emit('score', players);
+    
+    console_showPlayersArray();
+  });
+  
+  socket.on('flippedCard', function (data) {
+    console.log('> Card flipped \b \t \b \t ID : '+socket.id, data);
+    socket.broadcast.emit('flippedCard', data);
+  });
+  
+  socket.on('resetedCard', function (data) {
+    console.log('> Card reseted \b \t \b \t ID : '+socket.id, data);
+    socket.broadcast.emit('resetedCard', data);
+  });
+  
+  socket.on('foundCard', function (data) {
+    console.log('> Card found \b \t \b \t ID : '+socket.id, data);
+    socket.broadcast.emit('foundCard', data);
+  });
+  
+  socket.on('disconnect', function() {
+    for (var i = 0; i < players.length; i++) {
+      if (players[i].id == socket.id) {
+        // TODO : envoyer un message aux autres joueurs pour réinitialiser leur jeu (STATE_FORCE_RESTART)
+        
+        console.log('> Player left \b \t  \b \t ID : '+socket.id+' ('+i+')');
+        players.splice(i,1);
+        break; 
+      }
+    }
+  });
+});
 
-    socket.on( 'click', function( data ) {
-      console.log('click', data);
-        socket.broadcast.emit( 'new click', data );
-    } );
-
-    socket.on( 'foundByOther', function( data ) {
-        socket.broadcast.emit( 'foundByOther', data );
-    } );
-
-    socket.on ( 'foundOpponent', function( data ){
-        socket.broadcast.emit( 'foundOpponent', data);
-    } );
-
-    // when the user disconnects.. perform this
-    socket.on( 'disconnect', function( ) {
-    } );
-} );
+function console_showPlayersArray() {
+  console.log('+----------------------------------------------------------+');
+  
+  for (var i = 0; i < players.length; i++) {
+    console.log('+ '+players[i].id+' \b \t Ready to play: '+players[i].readyToPlay+'  \b \t Score : '+players[i].score+' +');
+  }
+  
+  console.log('+----------------------------------------------------------+');
+  console.log('');
+}
--- public/js/main3.js
+++ public/js/main3.js
@@ -1,225 +1,331 @@
-var totalPairs = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26'];
-var bg = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"];
-var colors1 = ["0, 0, 54", "0, 0, 122", "113, 10, 169", "0, 132, 108", "255, 16, 52"];
-var colors2 = ["255, 102, 102", "255, 197, 99", "255, 255, 116", "110, 151, 255", "159, 236, 255", "159, 236, 255", "160, 160, 160", "160, 128, 189", "100, 241, 142"];
+var nbPairs   = 27;   // Nombre de dossiers dans /data/pairs/
+var nbBg      = 41;   // Nombre de dossiers dans /data/bg/
+
+var STATE_INACTIVE = 0,
+    STATE_RULES = 1,
+    STATE_WAITING = 2,
+    STATE_PLAYING = 3,
+    STATE_WIN = 4,
+    STATE_LOST = 5;
+
+var STATE = STATE_INACTIVE;
+
+var totalPairs = [];
+
+for (var i = 1; i <= nbPairs; i++) {
+  totalPairs.push(i);
+}
+
+var colors1 = [
+  "0, 0, 54", 
+  "0, 0, 122", 
+  "113, 10, 169", 
+  "0, 132, 108", 
+  "255, 16, 52"
+];
+
+var colors2 = [
+  "255, 102, 102", 
+  "255, 197, 99", 
+  "255, 255, 116", 
+  "110, 151, 255", 
+  "159, 236, 255", 
+  "159, 236, 255", 
+  "160, 160, 160", 
+  "160, 128, 189", 
+  "100, 241, 142"
+];
+
 var color1 = colors1[Math.floor(Math.random() * colors1.length)];
 var color2 = colors2[Math.floor(Math.random() * colors2.length)];
 var rewardPatterns = ['0', '1', '2'];
 var rewardPattern = rewardPatterns[Math.floor(Math.random() * rewardPatterns.length)];
-console.log("valeur du rewardPattern : " + rewardPattern);
+
 var pairs = [];
-var actives = [];
+var visibleCards = 0;
 var randoms = [];
-var waitingOpponent = true;
 
-function getRandomIndex() {
-	var random = Math.floor(Math.random()*totalPairs.length);
-	if (randoms.indexOf(random) == -1) {
-		randoms.push(random);
-		return random;
-	} else {
-		return getRandomIndex();
-	}
+var player = {};
+
+var nbPairsPerPlayer = 9;
+
+function setStateTo(newState) {
+  $('.state').hide();
+  
+  switch (newState) {
+    case STATE_INACTIVE :
+      $('#inactive').show();
+      break;
+      
+    case STATE_RULES :
+      $('#rules').show();
+      break;
+      
+    case STATE_WAITING :
+      $('#waiting').show();
+      break;
+      
+    case STATE_PLAYING :
+      $('#game').show();
+      break;
+      
+    case STATE_WIN :
+      $('#win').show();
+      break;
+      
+    case STATE_LOST :
+      $('#lost').show();
+      break;
+      
+    default :
+      console.log('default');
+      break;
+  }
 }
 
-function get9RandomPairs(){
-	for(var i=0; i<9; i++){
-		var random = getRandomIndex();
-		pairs.push(random);
-	}
-	for(var i=0; i<pairs.length; i++){
-		pairs[i] = totalPairs[pairs[i]];
-	}
-	console.log(pairs);
-}
 
-function getBackgroundPic() {
-	var randomBg = Math.round(Math.random()*bg.length -1);
-	console.log(randomBg);
-	$('body').css({
-		"background" : "url('../data/bg/" + randomBg + ".jpg') no-repeat center fixed",
-		"background-size": "cover"
-	});
-	$('#inactive').css({
-		"background" : "url('../data/bg/" + randomBg + ".jpg') no-repeat center fixed",
-		"background-size": "cover"	
-	});
-	$('#rules').css({
-		"background" : "url('../data/bg/" + randomBg + ".jpg') no-repeat center fixed",
-		"background-size": "cover"	
-	});
-	$('#waiting').css({
-		"background" : "url('../data/bg/" + randomBg + ".jpg') no-repeat center fixed",
-		"background-size": "cover"	
-	});
-	$('#win').css({
-		"background" : "url('../data/bg/" + randomBg + ".jpg') no-repeat center fixed",
-		"background-size": "cover"	
-	});
-	$('#lose').css({
-		"background" : "url('../data/bg/" + randomBg + ".jpg') no-repeat center fixed",
-		"background-size": "cover"	
-	});
-	$('.colorBg').css("background", "rgba(" + color1 + ",0.4");
 
-}
 
-function shuffle(a) {
-    var j, x, i;
-    for (i = a.length; i; i -= 1) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-    }
-}
-
-$(document).ready(function(){
+$(document).ready(function() {
 	var socket = io();
-	socket.emit( 'join', ~~(Math.random()*10) );
-	socket.on( 'new click', function( data ) {
-	    console.log('new click', data.id );
-	    $(".card[data-order="+data.id+"]").toggleClass("visibleByOther");
-	} );
 
-	socket.on( 'foundByOther', function( data ) {
-	    console.log('foundByOther', data.id );
-	    $(".card[data-order="+data.id+"]").addClass("foundByOther");
-	} );
+  /*
+   *  Événements socket
+   */
+   
+  socket.on('newPlayer', function (data) {
+    player.id = data.id;
+    console.log('Player ID : '+player.id);
+  });
+	
+	socket.on('flippedCard', function (data) {
+	  console.log('Receive flippedCard : ', data.id);
+    $('.card[data-order='+data.id+']').addClass('visibleByOther');
+	});
+	
+	socket.on('resetedCard', function (data) {
+	  console.log('Receive resetedCard : ', data.id);
+    $('.card[data-order='+data.id+']').removeClass('visibleByOther');
+	});
 
-	socket.on( 'foundByOther', function( data ) {
-		$("#foundByOtherScore").html( "<strong>" + $(".foundByOther").length / 2 + "</strong> Adversaire" );
-	} );
+	socket.on('foundCard', function (data) {
+    console.log('Receive foundCard : ', data.id );
+    $(".card[data-order="+data.id+"]").addClass('foundByOther');
+	});
 
-	socket.on( 'found', function( data ) {
-		$("#foundScore").html( "Vous <strong>" + $(".found").length / 2 + "</strong> -" );
-	} );
+  socket.on('score', function (data) {
+    for (var i = 0; i < data.length; i++) {  
+      
+      /*
+       *  On s'intéresse au joueur courant
+       */   
+        
+      if (data[i].id == player.id) {
+        $('#foundScore strong, .scores_found').text(data[i].score);
+        
+        if (data[i].score == nbPairsPerPlayer) {
+          setStateTo(STATE_WIN);
+        }
+      } 
+      
+      /*
+       *  On s'intéresse aux autres joueurs
+       */
+      
+      else {
+        $('#foundByOtherScore strong, .scores_foundByOther').text(data[i].score);
+        
+        if (data[i].score == nbPairsPerPlayer) {
+          setStateTo(STATE_LOST);
+        }
+      }
+    }
+  });
 
 	$('body').css("color", "rgb("+ color2 +")");
 	$('path').css("fill", "rgb("+ color2 +")");
 
+  setStateTo(STATE_INACTIVE);
 
 	getBackgroundPic();
 
 	get9RandomPairs();
 
 	$('#inactive').on('click', function(){
-		$(this).css("visibility", "hidden");
-		$('#rules').css("visibility", "visible");
+		setStateTo(STATE_RULES);
 	});
 
-	$('#rules').on('click', function(){
-		$(this).css("visibility", "hidden");
-		if( waitingOpponent ) $('#waiting').css("visibility", "visible");
-		socket.emit( 'foundOpponent', null );
+	$('#rules').on('click', function(){		
+		setStateTo(STATE_WAITING);
+		
+		socket.emit('readyToPlay', true);
 	});
 
-	socket.on( 'foundOpponent', function() {
-		waitingOpponent = false;
-		$('#waiting').css("visibility", "hidden");
+	socket.on('startGame', function() {
+		setStateTo(STATE_PLAYING);
 	});
 
-	//création des cartes
-	for(var i = 0; i < pairs.length; i++){
-		// On crée les cartes qui vont accueillir la première image de chaque paire
-		var card0 = document.createElement('div');
-		card0.className += 'card';
-		card0.className += ' hidden';
-		card0.id = 'card0' + pairs[i];
-		$('#container').append(card0);
+	/*
+   *  Création des cartes
+   */
+	
+	for (var i = 0; i < pairs.length; i++){
+		
+		/*
+     *  On crée les cartes qui vont accueillir 
+     *  la première image de chaque paire
+		 */
+		 
+		var card0 = $('<div></div>')
+		  .attr('class','card')
+		  .attr('id','card0' + pairs[i])
+		  .attr('data-pair-id',pairs[i]);
+		$('#game').append(card0);
 
-		// On insère chaque première image dans les cartes
-		var img0 = document.createElement('img');
-		img0.id = 'img0' + pairs[i];
+		/*
+     *  On insère chaque première image 
+     *  dans les cartes
+     */
+     
+		var img0 = $('<img/>')
+		  .attr('id','img0' + pairs[i])
+		  .attr({'src':'../data/cards/' + pairs[i] + '/0.jpg'});
+		  
 		$('#card0' + pairs[i]).append(img0);
-		$('#img0' + pairs[i]).attr({
-			'src':"../data/" + pairs[i] + "/0.jpg"
-		});
+		  
 
-		// On crée les cartes qui vont accueillir la deuxième image de chaque paire
-		var card1 = document.createElement('div');
-		card1.className += 'card';
-		card1.className += ' hidden';
-		card1.id = 'card1' + pairs[i];
-		$('#container').append(card1);
+		/*
+     *  On crée les cartes qui vont accueillir 
+     *  la deuxième image de chaque paire
+     */
+		
+		var card1 = $('<div></div>')
+		  .attr('class','card')
+		  .attr('id','card1' + pairs[i])
+		  .attr('data-pair-id',pairs[i]);
+		$('#game').append(card1);
 
-		// On insère chaque deuxième image dans les cartes
-		var img1 = document.createElement('img');
-		img1.id = 'img1' + pairs[i];
+		/*
+     *  On insère chaque deuxième image 
+     *  dans les cartes
+     */
+     
+		var img1 = $('<img/>')
+		  .attr('id','img1' + pairs[i])
+		  .attr({'src':'../data/cards/' + pairs[i] + '/1.jpg'});
+		  
 		$('#card1' + pairs[i]).append(img1);
-		$('#img1' + pairs[i]).attr({
-			'src':"../data/" + pairs[i] + "/1.jpg"
-		});
 	}
 
 	var cards = $('.card');
 	shuffle(cards);
 	$('.card').remove();
 
-	for( var i=0, l = cards.length; i<l;i++){
-		$(cards[i]).attr("data-order", i);
-		$('#container').append(cards[i]);
+	for( var i = 0, l = cards.length; i < l; i++){
+		$(cards[i]).attr('data-order', i);
+		$('#game').append(cards[i]);
 	}
 
 	cards.on('click', function(){ 
-		$("#foundScore").html( "Vous <strong>" + $(".found").length / 2 + "</strong> -" );
-		// $("#foundByOtherScore").html( "Paires trouvées par l'adversaire : " + $(".foundByOther").length / 2 + " / 9" );
+  	
+    /*
+     *  S'il y a moins de 2 cartes au total retournées,
+     *  si la carte touchée n'est pas déjà retournée,
+     *  si la carte touchée n'est pas déjà trouvée
+     *  si la carte touchée n'est pas bloquée par l'autre joueur
+     */
 
+		if (($('.card.visible').length < 2) 
+		 && (!$(this).hasClass('visible'))
+		 && (!$(this).hasClass('found'))
+		 && (!$(this).hasClass('visibleByOther'))) {
+  		 
+      /*
+       *  On retourne la carte
+       *  et on envoie un événement socket
+       */
+  		 
+			$(this).toggleClass('visible');
+			
+			socket.emit('flippedCard', { id:$(this).attr('data-order') } );
 
-		if((! $(this).hasClass("visible")) && (actives.length < 2)  && (! $(this).hasClass("found") ) && (! $(this).hasClass("visibleByOther") ) ){
-			$(this).toggleClass("visible");
-			$('.visible').css("outline", "10px solid rgb(" + color2 + ")");
-			console.log('click', { id:$(this).attr("data-order") } );
-			socket.emit('click', { id:$(this).attr("data-order") } );
+      /*
+       *  S'il y a 2 cartes retournées,
+       *  on vérifie si c'est une paire ou non
+       */
 
-			actives = cards.filter(function(i, card){
-				return $(card).hasClass("visible");
-			});
-
-			// console.log( parseInt("" + this.id[this.id.length - 2] + this.id[this.id.length - 1] ) );
-
-			if( actives.length == 2 ){
-				window.setTimeout( function(){
+			if ($('.card.visible').length == 2) {
+				window.setTimeout(function() {
+  				console.log('Deux cartes retournées : on vérifie');
+  				
+  				// TODO
+  				
 					$('.visible').css("outline", "0");
 					$('.visibleByOther').css("outline", "solid 10px white");
-					$(actives[0]).toggleClass("visible");
-					$(actives[1]).toggleClass("visible");
-
-					if( Math.abs(parseInt("" + actives[0].id[actives[0].id.length - 3] + actives[0].id[actives[0].id.length - 2] + actives[0].id[actives[0].id.length - 1] ) - parseInt("" + actives[1].id[actives[1].id.length - 3] + actives[1].id[actives[1].id.length - 2] + actives[1].id[actives[1].id.length - 1] ) ) === 100 ){
-						console.log( "found pair" );
-						$(actives[0]).addClass("found").append('<div class="foundReward"></div>');
-						$(actives[1]).addClass("found").append('<div class="foundReward"></div>');
-						$('.foundReward').css("background", "url('../data/rewards/" + rewardPattern + ".png') no-repeat,  rgba(255,255,255,.3)");
-						/*socket.emit('click', function( data ){
-							  $(".card[data-order="+data.id+"]").toggleClass("foundByOther");
-						}*/
-						socket.emit('foundByOther', { id:$(actives[0]).attr("data-order") } );
-						socket.emit('foundByOther', { id:$(actives[1]).attr("data-order") } );
+					
+          /*
+           *  On compare l'identifiant de la paire [data-pair-id="X"]
+           *  de toutes les .card.visible pour vérifier la paire ;
+           *  l'identifiant de la première carte sert de comparatif
+           */
+					
+					var pairIdToFetch = $('.card.visible').eq(0).data('pair-id');
+					var isPair = true;
+										
+					for (var i = 1; i < $('.card.visible').length; i++) {
+  					if ($('.card.visible').eq(i).data('pair-id') != pairIdToFetch) {
+    					isPair = false;
+  					}
 					}
-					// else{
-						socket.emit('click', { id:$(actives[0]).attr("data-order") } );
-						socket.emit('click', { id:$(actives[1]).attr("data-order") } );
-					// }
+					
+					/*
+  				 *  Si on a une paire
+  				 */
 
-					actives = cards.filter(function(i, img){
-						return $(img).hasClass("visible");
-					});
+					if (isPair) {
+						console.log('Paire trouvée');
+						
+						$('.card.visible').addClass('found').append('<div class="foundReward"></div>');						
+						$('.foundReward').css("background", "url('../data/rewards/" + rewardPattern + ".png') no-repeat, rgba(255,255,255,.3)");
+						
+						socket.emit('foundPair', {pair: pairIdToFetch});			
+									
+            for (var i = 0; i < $('.card.visible').length; i++) {
+						  socket.emit('foundCard', { id: $('.card.visible').eq(i).attr('data-order') });
+						}
+					}
+					
+					/*
+  				 *  On libère les 2 cartes pour les autres joueurs
+  				 */
+					
+          for (var i = 0; i < $('.card.visible').length; i++) {
+					  socket.emit('resetedCard', { id:$('.card.visible').eq(i).attr('data-order') });
+					}
+						
+          $('.card.visible').removeClass('visible');
 
-				}, 800 );
+				}, 800);
 			}
 		}
+		
+		/*
 
-		if( $(".found").length == 18 ){
+		if ($('.found').length == 18) {
 			$("#win").css("visibility", "visible");
 			$("#win h2").append("<h3><strong>9 à " + $('.foundByOther').length / 2 + "</strong></h3>");
+      $('#game').hide();
 			window.setTimeout( function(){ location.assign(location); }, 30000 );
 		}
 
 		if( $(".foundByOther").length == 18 ){
 			$("#lose").css("visibility", "visible");
 			$("#lose h2").append("<h3><strong>9 à " + $('.found').length / 2 + "</strong></h3>");
+      $('#game').hide();
 			window.setTimeout( function(){ location.assign(location); }, 30000 );
 		}
+		
+		*/
 
 	});
 
@@ -229,3 +335,73 @@ $(document).ready(function(){
 
 
 });
+
+/*
+ *  getRandomIndex()
+ *
+ *  Extrait une valeur aléatoire unique issue du tableau `totalPairs`
+ *  (il ne peut pas y avoir de doublon dans les multiples appels de getRandomIndex() )
+ */
+
+function getRandomIndex() {
+	var random = Math.floor(Math.random()*nbPairs);
+	
+	if (randoms.indexOf(random) == -1) {
+		randoms.push(random);
+		return random;
+	} else {
+		return getRandomIndex();
+	}
+}
+
+/*
+ *  get9RandomPairs()
+ *
+ *  Récolte 9 paires 
+ */
+
+function get9RandomPairs(){
+	for (var i = 0; i < 9; i++) {
+		var random = getRandomIndex();
+		pairs.push(random);
+	}
+	
+	for (var i = 0; i < pairs.length; i++){
+		pairs[i] = totalPairs[pairs[i]];
+	}
+}
+
+/*
+ *  getBackgroundPic()
+ *
+ *  Attribue une image de fond aléatoirement choisie
+ *  et une teinte aléatoirement choisie
+ */
+
+function getBackgroundPic() {
+	var randomBg = Math.round(Math.random()*nbBg);
+	
+	$('.imageBg').css({
+		"background" : "url('../data/bg/" + randomBg + ".jpg') no-repeat center center / cover"
+	});
+	
+	$('.colorBg').css({
+  	"background" : "rgba(" + color1 + ",0.4)"
+	});
+}
+
+/*
+ *  shuffle(a)
+ *
+ *  @param  a (type: Array)
+ */
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length; i; i -= 1) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
+}
