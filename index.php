8cc37874b4ed2b88311bf7ba77e017f4317a28f8
racine-p-a <pierrealexandreracine@gmail.com>
2020-09-02 10:32:47 +0200

    Algorithme côté serveur.

--- index.php
+++ index.php
@@ -12,3 +12,19 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+/*
+ * Pour une compréhension globale du fonctionnement du prototype, référez-vous eu readme.md
+ *
+ * Cette page contient le code du serveur. Celui-ci doit suivre l'algorithme suivant :
+ *
+ * - charger le fichier de la configuration voulue (configuration.json).
+ * - attendre de recevoir un signal de départ voulu par les deux machines-joueuses.
+ * - si deux messages de départ ont été émis, le serveur leur répondra que le jeu peut commencer tout en fournissant
+ * les données d'image.
+ * - si un message de victoire est envoyé par une des machines-joueuses, on annonce à l'autre qu'elle a perdu.
+ *
+ *
+ * NOTE : quand une machine-joueuse désire jouer, elle fait un appel Ajax toutes les secondes et c'est lors de la
+ * réponse à ces requêtes qu'on peut leur faire passer des messages comme les images ou les notification de victoire.
+ */
